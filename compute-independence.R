7af0b0707e80d2745dc32f4db512c5e7970d64b3
Sebastian Sauer <ssauer@posteo.de>
2022-01-28 13:52:04 +0100

    update code

--- compute-independence.R
+++ compute-independence.R
@@ -9,41 +9,12 @@ library(tidyverse)
 library(tictoc)  # time measurement of computation durating, not strictly needed
 
 
-# download data from osf:
-d <- read_csv("https://osf.io/cjxua/?action=download")
-
 # codebook:
 codebook_url <- "https://osf.io/v68t9/"
 
 
 vars_expected_independent <-
   c("Dem_gender", "Dem_age")
-
-
-d <- read_csv("d3.csv")
-
-d3 <-
-  d %>%
-  select(all_of(vars_expected_independent)) %>%
-  mutate(is_female = ifelse(Dem_gender == "Female", 1, 0)) %>%
-  select(-Dem_gender) %>%
-  drop_na() %>%
-  mutate(across(everything(),  #z-scaling
-                ~ ((. - mean(.))/sd(.))))
-
-
-write_csv(d3, file = "d3.csv")
-
-
-d4 <-
-  d %>%
-  group_by(Country) %>%
-  mutate(ns = n()) %>%
-  filter(ns>=30) #%>% #Lieberoth et al. (2021)
-  filter(Duration..in.seconds.>= 132) %>%
-  ungroup() %>%
-  select(Dem_gender, Dem_age, gain_safe, PSS10_avg, neu, ext, ope, agr, con) %>%
-  na.omit() #third exclusion criterion
 
 
 options(mc.cores = parallel::detectCores())
