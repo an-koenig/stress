065e4398c300acd0bf65ee6c245d5830b2162086
Sebastian Sauer <ssauer@posteo.de>
2022-01-28 13:49:51 +0100

    use Adriana's dataset

--- compute-independence.R
+++ compute-independence.R
@@ -20,6 +20,8 @@ vars_expected_independent <-
   c("Dem_gender", "Dem_age")
 
 
+d <- read_csv("d3.csv")
+
 d3 <-
   d %>%
   select(all_of(vars_expected_independent)) %>%
@@ -33,14 +35,37 @@ d3 <-
 write_csv(d3, file = "d3.csv")
 
 
+d4 <-
+  d %>%
+  group_by(Country) %>%
+  mutate(ns = n()) %>%
+  filter(ns>=30) #%>% #Lieberoth et al. (2021)
+  filter(Duration..in.seconds.>= 132) %>%
+  ungroup() %>%
+  select(Dem_gender, Dem_age, gain_safe, PSS10_avg, neu, ext, ope, agr, con) %>%
+  na.omit() #third exclusion criterion
+
+
 options(mc.cores = parallel::detectCores())
+
+d5 <- read_csv("COVIDiSTRESS_stress5.csv")
+
+d6 <-
+  d5 %>%
+  select(all_of(vars_expected_independent)) %>%
+  mutate(is_female = ifelse(Dem_gender == "Female", 1, 0)) %>%
+  select(-Dem_gender) %>%
+  drop_na() %>%
+  mutate(across(everything(),  #z-scaling
+                ~ ((. - mean(.))/sd(.))))
 
 tic()
 # note that a simple regression with standardized variables is identical to a correlation
-m1 <- stan_glm(Dem_age ~ is_female, data = d3)
+m1 <- stan_glm(Dem_age ~ is_female, data = d6)
 toc()
 
-write_rds(m1, file = "m1.rds")
+write_rds(m1, file = "m1_v2.rds")
 
 m1
+
 posterior_interval(m1, pars = "is_female", prob = .95)
